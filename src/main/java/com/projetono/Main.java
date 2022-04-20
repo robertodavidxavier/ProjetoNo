67a891fc31bf9838b3abd6763c54501e02736523
robertodavidxavier <davidxaviersilva@gmail.com>
2022-04-20 11:24:32 -0300

    Refatoração Projeto Nó

--- src/main/java/com/projetono/Main.java
+++ src/main/java/com/projetono/Main.java
@@ -3,15 +3,15 @@ package com.projetono;
 public class Main {
 
     public static void main(String[] args){
-        No no1 = new No("conteúdo no1");
+        No<String> no1 = new No<>("conteúdo no1");
 
-        No no2 = new No("conteúdo no2");
+        No<String> no2 = new No<>("conteúdo no2");
         no1.setProximoNo(no2);
 
-        No no3 = new No("conteúdo no3");
+        No<String> no3 = new No<>("conteúdo no3");
         no2.setProximoNo(no3);
 
-        No no4 = new No("conteúdo no4");
+        No<String> no4 = new No<>("conteúdo no4");
         no3.setProximoNo(no4);
 
         //no1->no2->no3->no4->null
